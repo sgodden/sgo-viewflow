42f3add9d3acec7f0f08c14886823feaa1de8d3e
ian.cumberland@gmail.com <ian.cumberland@gmail.com@cd2333e4-1219-11de-8784-2f9f11b02f98>
2009-08-27 13:52:43 +0000

    Add methods canClose and prepareForClose

--- trunk/src/main/java/org/sgodden/viewflow/CloseableView.java
+++ trunk/src/main/java/org/sgodden/viewflow/CloseableView.java
@@ -2,15 +2,30 @@ package org.sgodden.viewflow;
 
 /**
  * Denotes the capability of a view to be asked to close.
+ * 
  * @author Lloyd Colling
- *
+ * 
  */
 public interface CloseableView extends View {
 
-    /**
-     * Asks the view to close, with a false return value
-     * indicating that the view cannot be closed.
-     * @return
-     */
-    public boolean closeView();
+	/**
+	 * Asks the view to close, with a false return value indicating that the
+	 * view cannot be closed.
+	 * 
+	 * @return
+	 */
+	public boolean closeView();
+
+	/**
+	 * Determines if the view can be closed
+	 * 
+	 * @return
+	 */
+	public boolean canClose();
+
+	/**
+	 * Gets the view to do anything that will allow closeView() to return true,
+	 * and returns true if it succeeds, and false if it does not succeed
+	 */
+	public boolean prepareForClose();
 }
